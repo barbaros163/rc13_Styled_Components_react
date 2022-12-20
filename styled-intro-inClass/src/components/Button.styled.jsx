f20983f080ae81910a63953a78f9532d4eb67eff
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-20 22:42:55 +0100

    tomato

--- styled-intro-inClass/src/components/Button.styled.jsx
+++ styled-intro-inClass/src/components/Button.styled.jsx
@@ -8,10 +8,16 @@ export const Button = styled.button`
   cursor: pointer;
   padding: 1rem 2rem;
   border-radius: 0.5rem;
-  border-color: purple;
+  border-color: 1px solid purple;
   font-size: 1.1rem;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   &:hover {
     transform: scale(0.97);
   }
+`;
+
+export const TomatoButton = styled(Button)`
+  background-color: ${({ primary }) => (primary ? "white" : "tomato")};
+  color: ${({ primary }) => (primary ? "tomato" : "white")};
+  border-color: 1px solid tomato;
 `;
