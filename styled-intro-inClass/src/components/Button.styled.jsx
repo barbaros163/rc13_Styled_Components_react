cfe625a454404b5e1f9d91b402ae2bb1ec262eef
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-20 22:34:16 +0100

    props

--- styled-intro-inClass/src/components/Button.styled.jsx
+++ styled-intro-inClass/src/components/Button.styled.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  background-color: hotpink;
-  color: white;
+  /* background-color: hotpink; */
+  /* color: white; */
+  background-color: ${(props) => (props.primary ? "white" : "purple")};
+  color: ${({ primary }) => (primary ? "purple" : "white")};
   cursor: pointer;
   padding: 1rem 2rem;
   border-radius: 0.5rem;
-  border-color: pink;
+  border-color: purple;
+  font-size: 1.1rem;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   &:hover {
     transform: scale(0.97);
   }
