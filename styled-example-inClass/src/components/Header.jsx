ad2e955b5762562a992ddbabf1df22462d7c78c4
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-21 22:28:05 +0100

    theme props added

--- styled-example-inClass/src/components/Header.jsx
+++ styled-example-inClass/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import Button from "./styles/Button.styled";
+import StyledHeader from "./styles/Header.styled";
 
 const Header = () => {
   return (
-    <div>
+    <StyledHeader>
       <Button color="#a62440">Apply Courses</Button>
       <Button bg="#a62440">Talk to Adviser</Button>
-    </div>
+    </StyledHeader>
   );
 };
 
