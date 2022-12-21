11d18b4f2ef4987840d830a07db4a07115e1fd2e
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-21 17:38:39 +0100

    buttons

--- styled-example-inClass/src/components/styles/Button.styled.jsx
+++ styled-example-inClass/src/components/styles/Button.styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Button = styled.button`
   background: ${({ bg }) => bg || "white"};
   color: ${({ color }) => color || "white"};
-  border: 1px solid #af3a53;
+  border: 1px solid #a62440;
 `;
 
 export default Button;
