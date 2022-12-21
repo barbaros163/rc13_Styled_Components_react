893e4a93d35906301ddd579820a1e34669b4f53e
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-21 15:47:18 +0100

    link

--- styled-intro-inClass/src/App.js
+++ styled-intro-inClass/src/App.js
@@ -1,14 +1,22 @@
 import { Button, TomatoButton } from "./components/Button.styled";
 import Container from "./components/Container.styled";
+import HeaderText from "./components/HeaderText.styled";
+import StyledLink from "./components/Link.styled";
 
 const App = () => {
   return (
-    <Container bg="grey">
-      <Button primary>Save</Button>
-      <Button>Clear</Button>
-      <TomatoButton>Send</TomatoButton>
-      <TomatoButton primary>Submit</TomatoButton>
-    </Container>
+    <>
+      <Container bg="grey">
+        <HeaderText color="hotpink">Styled Components</HeaderText>
+        <Button primary>Save</Button>
+        <Button>Clear</Button>
+        <TomatoButton>Send</TomatoButton>
+        <TomatoButton primary>Submit</TomatoButton>
+      </Container>
+      <Container>
+        <StyledLink href="https://clarusway.com/" target="_blank"></StyledLink>
+      </Container>
+    </>
   );
 };
 
