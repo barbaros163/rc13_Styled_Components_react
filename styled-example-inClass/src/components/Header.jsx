db24bc2b6c8cfc99e2231089d3c2f1111522b1ae
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-21 23:10:53 +0100

    header part 1 done

--- styled-example-inClass/src/components/Header.jsx
+++ styled-example-inClass/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import Button from "./styles/Button.styled";
-import StyledHeader from "./styles/Header.styled";
+import StyledHeader, { Logo, Nav } from "./styles/Header.styled";
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Button color="#a62440">Apply Courses</Button>
-      <Button bg="#a62440">Talk to Adviser</Button>
+      <Nav>
+        <Logo src="./images/logo.png" />
+        <div>
+          <Button color="#a62440">Apply Courses</Button>
+          <Button bg="#a62440">Talk to Adviser</Button>
+        </div>
+      </Nav>
     </StyledHeader>
   );
 };
